d9e76bb9ac5f64686be6062d91aa054888bd03ab
username <wecp-dummy@gmail.com>
2024-01-27 12:25:21 +0000

    Day2 Commit

--- backend/src/main/java/com/wecp/progressive/entity/Transactions.java
+++ backend/src/main/java/com/wecp/progressive/entity/Transactions.java
@@ -1,63 +1,64 @@
 package com.wecp.progressive.entity;
 import java.util.Date;
 
-
 public class Transactions {
-    private int transaction_id;
-    private int account_id;
+
+    private int transactionId;
+    private int accountId;
     private double amount;
-    private String transaction_date;
-    private String transaction_type;
+    private String transactionType;
+    private Date transactionDate;
 
-    public Transactions(){
+    public Transactions() {
+        //
+    }
 
+    public Transactions(int transactionId, int accountId, double amount, Date timestamp, String transactionType) {
+        this.transactionId = transactionId;
+        this.accountId = accountId;
+        this.amount = amount;
+        this.transactionDate = timestamp;
+        this.transactionType = transactionType;
     }
-    public Transactions(int transaction_id,int account_id,double amount,String transaction_date,String transaction_type){
-        this.transaction_id=transaction_id;
-        this.account_id=account_id;
-        this.amount=amount;
-        this.transaction_date=transaction_date;
-        this.transaction_type=transaction_type;
 
+    // Getters and setters
+    public int getTransactionId() {
+        return transactionId;
+    }
 
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
     }
-    public int getTransaction_id() {
-        return transaction_id;
+
+    public int getAccountId() {
+        return accountId;
     }
-    public void setTransaction_id(int transaction_id) {
-        this.transaction_id = transaction_id;
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
     }
-    public int getAccount_id() {
-        return account_id;
-    }
-    public void setAccount_id(int account_id) {
-        this.account_id = account_id;
-    }
+
     public double getAmount() {
         return amount;
     }
+
     public void setAmount(double amount) {
         this.amount = amount;
     }
-    public String getTransaction_date() {
-        return transaction_date;
-    }
-    public void setTransaction_date(String transaction_date) {
-        this.transaction_date = transaction_date;
-    }
-    public String getTransaction_type() {
-        return transaction_type;
-    }
-    public void setTransaction_type(String transaction_type) {
-        this.transaction_type = transaction_type;
-    }
-    @Override
-    public String toString() {
-        return "Transactions [transaction_id=" + transaction_id + ", account_id=" + account_id + ", amount=" + amount
-                + ", transaction_date=" + transaction_date + ", transaction_type=" + transaction_type + "]";
-    }
-    
-    
 
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
 
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
 }
