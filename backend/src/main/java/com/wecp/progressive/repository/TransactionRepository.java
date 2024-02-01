3913b1c88e4d72bc30eea109fc2479b1c51806ce
username <wecp-dummy@gmail.com>
2024-02-01 17:50:35 +0000

    day8

--- backend/src/main/java/com/wecp/progressive/repository/TransactionRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TransactionRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Integer> {
 
+    List<Transactions> findByAccountsAccountId(int accountId);
 }
 
